a7152cbc75a9207a8f39f89a7ac27c804d3758d9
Kolby <mckeownkolby@gmail.com>
2020-10-01 11:41:31 -0400

    Width Bug Fix

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import data from '../utils/data.json'
+import React from "react";
+import styled from "styled-components";
+import data from "../utils/data.json";
 
 const Banner = () => {
-    return (
-        <Wrapper>
-            <Img src={data.banner.url} />
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <Img src={data.banner.url} />
+    </Wrapper>
+  );
+};
 
 export default Banner;
 
 const Wrapper = styled.div`
-    width: 100vw;
-`
+  width: 100%;
+`;
 const Img = styled.img`
-    width: 100vw;
+  width: 100%;
 `;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import FooterImg from '../Footer.png'
-import data from '../utils/data.json'
+import React from "react";
+import styled from "styled-components";
+import FooterImg from "../Footer.png";
+import data from "../utils/data.json";
 
 const Footer = () => {
-    return (
-        <Wrapper>
-            <Img src={data.footer.img} />
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <Img src={data.footer.img} />
+    </Wrapper>
+  );
+};
 
 export default Footer;
 
 const Wrapper = styled.div`
-    height: 500px;
-    margin-top: 25px;
+  height: 500px;
+  margin-top: 25px;
+  width: 100%;
 `;
 
 const Img = styled.img`
-    height: 500px;
-    width: 100vw;
+  height: 500px;
+  width: 100%;
 `;
