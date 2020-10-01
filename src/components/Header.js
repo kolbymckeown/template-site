ed671953d647e689380e4d5dc9530d629305d07e
Kolby <mckeownkolby@gmail.com>
2020-10-01 11:41:31 -0400

    Width Bug Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { BsSearch, BsBag } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
-import data from '../utils/data.json'
+import data from "../utils/data.json";
 
 const Header = () => {
   return (
@@ -12,29 +12,29 @@ const Header = () => {
       </Top>
       <Middle>
         <Lang>
-        <p>{data.header.center.languages}</p>
+          <p>{data.header.center.languages}</p>
         </Lang>
         <Logo>
           <Img src={data.header.center.logo} />
         </Logo>
-        <div style={{display: 'flex'}}>
-        <Search>
-          <BsSearch style={{marginRight: '5px'}} />
-          <Input type="text" placeholder="Search" />
-        </Search>
+        <div style={{ display: "flex" }}>
+          <Search>
+            <BsSearch style={{ marginRight: "5px" }} />
+            <Input type="text" placeholder="Search" />
+          </Search>
 
-        <Div>
-          <AiOutlineHeart />
-          <BsBag style={{marginLeft: '8px'}}/>
-        </Div>
+          <Div>
+            <AiOutlineHeart style={{ marginLeft: "5px" }} />
+            <BsBag style={{ marginLeft: "5px" }} />
+          </Div>
         </div>
       </Middle>
       <Bottom>
         <Nav>
-            <A href="#">{data.header.categories.option1}</A>
-            <A href="#">{data.header.categories.option2}</A>
-            <A href="#">{data.header.categories.option3}</A>
-            <A href="#">{data.header.categories.option4}</A>
+          <A href="#">{data.header.categories.option1}</A>
+          <A href="#">{data.header.categories.option2}</A>
+          <A href="#">{data.header.categories.option3}</A>
+          <A href="#">{data.header.categories.option4}</A>
         </Nav>
       </Bottom>
     </Wrapper>
@@ -43,9 +43,7 @@ const Header = () => {
 
 export default Header;
 
-const Wrapper = styled.div`
-    
-`;
+const Wrapper = styled.div``;
 
 const Div = styled.div`
   font-size: 22px;
@@ -63,6 +61,7 @@ const Top = styled.div`
 
 const Img = styled.img`
   height: 40px;
+  margin-left: 70px;
 `;
 
 const Middle = styled.div`
@@ -74,7 +73,7 @@ const Middle = styled.div`
 `;
 
 const Lang = styled.div`
-    font-size: 12px;
+  font-size: 12px;
 `;
 
 const Logo = styled.div`
@@ -82,13 +81,13 @@ const Logo = styled.div`
   justify-content: center;
   text-align: center;
   flex-direction: column;
-    margin-left: 130px;
+  margin-left: 130px;
 `;
 
 const Search = styled.div`
-    border-bottom: 1px solid black;
-    font-size: 20px;
-    position: relative;
+  border-bottom: 1px solid black;
+  font-size: 20px;
+  position: relative;
 `;
 
 const Input = styled.input`
@@ -96,20 +95,20 @@ const Input = styled.input`
 `;
 
 const Bottom = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    background: ${data.header.categories.backgroundColor};
-    padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: ${data.header.categories.backgroundColor};
+  padding: 10px;
 `;
 
 const Nav = styled.nav`
-    font-size: 14px;
+  font-size: 14px;
 `;
 
 const A = styled.a`
-    padding: 28px;
-    text-decoration: none;
-    color: ${data.header.categories.fontColor};
+  padding: 28px;
+  text-decoration: none;
+  color: ${data.header.categories.fontColor};
 `;
